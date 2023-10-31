a57d1748f0da85a01d2ba10ef80d1efff5af8722
Diego Canteros <diegoelchivo87@hotmail.com>
2023-10-31 20:52:14 -0300

    Creacion de migraciones

---
+++ database/migrations/2023_10_31_234030_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('order_ref');
+            $table->string('customer_name');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
